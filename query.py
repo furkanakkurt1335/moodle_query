402455fddb01fd3c712a801c5cffb36e18cf614e
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-06-11 12:34:37 +0300

    update script

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-import requests, re, os, json, argparse
+import requests, re, os, json
 from bs4 import BeautifulSoup
 from datetime import datetime
 import logging
@@ -8,39 +8,6 @@ THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 log_path = os.path.join(THIS_DIR, 'changes.log')
 logging.basicConfig(filename=log_path, level=logging.INFO, format='%(asctime)s %(message)s')
 logger = logging.getLogger(__name__)
-
-parser = argparse.ArgumentParser(description='Query Moodle for changes in course pages and grades.')
-parser.add_argument('-s', '--semester', type=str, help='Semester to query, e.g. 2022/2023-2')
-args = parser.parse_args()
-
-sem_pattern = r'^\d{4}/\d{4}-\d$'
-
-data_path = os.path.join(THIS_DIR, 'data.json')
-if not os.path.exists(data_path):
-    if args.semester:
-        semester = args.semester
-        sem_search = re.search(sem_pattern, semester)
-        if not sem_search:
-            print('Invalid semester format in arguments. Example: 2022/2023-2')
-            exit()
-        else:
-            semester = sem_search.group(0)
-            with open(data_path, 'w', encoding='utf-8') as f:
-                json.dump({'semester': semester}, f)
-    else:
-        semester = input('Semester: ')
-        sem_search = re.search(sem_pattern, semester)
-        if not sem_search:
-            print('Invalid semester format input. Example: 2022/2023-2')
-            exit()
-        else:
-            semester = sem_search.group(0)
-            with open(data_path, 'w', encoding='utf-8') as f:
-                json.dump({'semester': semester}, f)
-else:
-    with open(data_path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        semester = data['semester']
 
 credentials_path = os.path.join(THIS_DIR, 'credentials.json')
 if not os.path.exists(credentials_path):
@@ -55,7 +22,7 @@ with open(credentials_path, 'r', encoding='utf-8') as f:
         exit()
 
 urls_path = os.path.join(THIS_DIR, 'URLs.json')
-empty_urls_d = { "Login": "https://moodle.boun.edu.tr/login/index.php", "Dashboard": "https://moodle.boun.edu.tr/my/", "Pages": {} }
+empty_urls_d = { "login": "https://moodle.boun.edu.tr/login/index.php", "dashboard": "https://moodle.boun.edu.tr/my/", "pages": dict() }
 if not os.path.exists(urls_path):
     with open(urls_path, 'w', encoding='utf-8') as f:
         json.dump(empty_urls_d, f)
@@ -65,7 +32,7 @@ else:
             urls = json.load(f)
         except:
             urls = {}
-    keys = ['Dashboard', 'Login', 'Pages']
+    keys = ['dashboard', 'login', 'pages']
     for k in keys:
         if k not in urls.keys():
             with open(urls_path, 'w', encoding='utf-8') as f:
@@ -79,28 +46,35 @@ if not os.path.exists(pages_folder):
     os.mkdir(pages_folder)
 
 sess = requests.session()
-login_post = sess.post(urls['Login'], data=data)
+login_post = sess.post(urls['login'], data=data)
 
 def get_pages(sess):
-    urls['Pages'] = {}
-    course_pattern = f'{semester} (.*?)$'
-    dashboard_get = sess.get(urls['Dashboard'])
+    urls['pages'] = dict()
+    course_pattern = '(\d{4}/\d{4}-\d) (.*?)$'
+    dashboard_get = sess.get(urls['dashboard'])
     dashboard_text = dashboard_get.text
     soup = BeautifulSoup(dashboard_text, 'html.parser')
     courses = soup.find('select', attrs={'name': 'course'}).find_all('option')
+    sem_s = set()
     for course in courses:
         course_find = re.search(course_pattern, course.text)
         if course_find:
-            course_code = course_find.group(1)
+            sem = course_find.group(1)
+            if len(sem_s) == 0:
+                print('Semester:', sem)
+            sem_s.add(sem)
+            if len(sem_s) > 1:
+                break
+            course_code = course_find.group(2)
             course_id = course['value']
-            d_t = {'course_code': course_code, 'course_id': course_id, 'course_url': f'https://moodle.boun.edu.tr/course/view.php?id={course_id}', 'grade_url': f'https://moodle.boun.edu.tr/grade/report/user/index.php?id={course_id}'}
-            urls['Pages'][course_id] = d_t
+            d_t = {'course_code': course_code, 'course_id': course_id, 'course_url': 'https://moodle.boun.edu.tr/course/view.php?id={}'.format(course_id), 'grade_url': 'https://moodle.boun.edu.tr/grade/report/user/index.php?id={}'.format(course_id)}
+            urls['pages'][course_id] = d_t
     with open(urls_path, 'w', encoding='utf-8') as f:
         json.dump(urls, f, indent=4, ensure_ascii=False)
-    return urls['Pages']
+    return urls['pages']
 
-if urls['Pages'] != {}:
-    pages = urls['Pages']
+if urls['pages']:
+    pages = urls['pages']
 else:
     pages = get_pages(sess)
 
@@ -126,25 +100,27 @@ def check_change(sess, pages):
         topics = soup.find('ul', class_='topics')
         course_content = None
         if weeks:
-            soup.find('span', class_='order-1').decompose()
+            span_order_t = soup.find('span', class_='order-1')
+            if span_order_t:
+                span_order_t.decompose()
             course_content = str(weeks).replace('\n\n', '\n')
         elif topics:
             course_content = str(topics)
         if course_content == None:
-            print(f'{course["course_code"]} Course Page is not available')
+            print('Course page of %s is not available' % course['course_code'])
         else:
             filepath = os.path.join(pages_folder, '{cc}_course.html'.format(cc=course['course_code']))
             if not os.path.exists(filepath):
                 with open(filepath, 'w', encoding='utf-8') as f:
                     f.write(course_content)
-                    print_str = '{cc} Course Page created'.format(cc=course['course_code'])
+                    print_str = 'Course page of %s created' % course['course_code']
                     print(print_str, ts)
                     logger.info(print_str)
             else:
                 with open(filepath, 'r', encoding='utf-8') as f:
                     previous_content = f.read()
                 if course_content != previous_content:
-                    print_str = '{cc} Course Page changed'.format(cc=course['course_code'])
+                    print_str = 'Course page of %s changed' % course['course_code']
                     print(print_str, ts)
                     logger.info(print_str)
                     # beep(sound=1)
@@ -160,7 +136,7 @@ def check_change(sess, pages):
             act.decompose()
         grade_table = soup.find('table', class_='user-grade')
         if grade_table == None:
-            print(f'{course["course_code"]} Grade Page is not available')
+            print('Grade page of %s is not available' % course['course_code'])
             continue
         else:
             grade_table = str(grade_table)
@@ -168,14 +144,14 @@ def check_change(sess, pages):
             if not os.path.exists(filepath):
                 with open(filepath, 'w', encoding='utf-8') as f:
                     f.write(grade_table)
-                    print_str = '{cc} Grade Page created'.format(cc=course['course_code'])
+                    print_str = 'Grade page of %s created' % course['course_code']
                     print(print_str, ts)
                     logger.info(print_str)
             else:
                 with open(filepath, 'r', encoding='utf-8') as f:
                     previous_content = f.read()
                 if grade_table != previous_content:
-                    print_str = '{cc} Grade Page changed'.format(cc=course['course_code'])
+                    print_str = 'Grade page of %s changed' % course['course_code']
                     print(print_str, ts)
                     logger.info(print_str)
                     # beep(sound=1)
